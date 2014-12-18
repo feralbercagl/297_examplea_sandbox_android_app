d66b22e2c89da0e8a5c9f3869434f52edc87951f
Android Generator <android_generator@anypresence.com>
2014-12-18 16:47:27 +0000

    Purging existing sources.

--- ExampleaProject/Examplea/src/com/anypresence/android/examplea/ISimpleSwapFragmentListener.java
+++
@@ -1,9 +0,0 @@
-package com.anypresence.android.examplea;
-
-import android.support.v4.app.Fragment;
-import java.util.List;
-import com.anypresence.rails_droid.RemoteObject;
-
-public interface ISimpleSwapFragmentListener {
-	void swapFragment(Fragment newFragment);
-}
